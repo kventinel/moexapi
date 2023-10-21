02496672c022f2d40d8d8d99b5fab3bce43b2f70
Aliaksei Rak <kventinel@gmail.com>
2023-10-22 00:19:05 +0300

    update boards parsing

--- moexapi/boards.py
+++ moexapi/boards.py
@@ -15,10 +15,13 @@ def get_main_board(boards: list[str]) -> str:
         return boards[0]
     NAMES = (
         ("TQCB", "TQBR", "TQTF", "CETS", "TQIF"),
-        ("TQOB", "TQIR", "TQRD"),
+        ("TQOB", "TQIR", "TQRD", "TQPI"),
     )
     for tier in NAMES:
         board = _check_tier(boards, tier)
         if board:
             return board
-    raise RuntimeError(f"Can't compare boards {boards}")
+    raise RuntimeError(
+        f"Can't compare boards {boards}, you can find some info about boards at "
+        "https://iss.moex.com/iss/engines/stock/markets/bonds/boards"
+    )
