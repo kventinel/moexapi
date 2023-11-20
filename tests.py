0725dba7e4d85ef6939a6fba229bbf034de079fe
Aliaksei Rak <kventinel@gmail.com>
2023-11-20 12:21:28 +0300

    add tests

---
+++ tests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import unittest
+
+import moexapi
+
+
+class GetCandles(unittest.TestCase):
+    def test_index(self):
+        ticker = moexapi.get_ticker("IMOEX")
+        candles = moexapi.get_candles(ticker)
+        self.assertGreater(len(candles), 0)
+
+    def test_share(self):
+        ticker = moexapi.get_ticker("GAZP")
+        candles = moexapi.get_candles(ticker)
+        self.assertGreater(len(candles), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
